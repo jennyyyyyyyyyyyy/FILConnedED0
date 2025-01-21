13ddd9a4dbe132437fcd39bced4cd295eefc6168
jennyyyyyyyyyyyy <153977628+jennyyyyyyyyyyyy@users.noreply.github.com>
2025-01-21 23:37:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fetch = require('isomorphic-fetch'); // Required for Node.js
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.get("/oauth2redirect", async (req, res) => {
   const code = req.query.code;  // Get the authorization code from query params
   if (!code) {
@@ -27,10 +28,10 @@ app.get("/oauth2redirect", async (req, res) => {
     );
     // Extract the access token from Dropbox's response
     const accessToken = response.data.access_token;
+
     // Log the access token received from Dropbox
     console.log("Access Token received:", accessToken);
-
-    // Initialize Dropbox client with the access token
+// Initialize Dropbox client with the access token
     const dbx = new Dropbox({ accessToken: accessToken, fetch: fetch });
 
     // Retrieve and log the user's account information, including permissions
@@ -42,8 +43,7 @@ app.get("/oauth2redirect", async (req, res) => {
       .catch((error) => {
         console.error('Error retrieving account info:', error);
       });
-
-    // Redirect to the mobile app with the access token
+  // Redirect to the mobile app with the access token
     const redirectUri = `filconnected://oauth2redirect?access_token=${accessToken}`; // Use your custom scheme
     res.redirect(redirectUri); // Redirect to the custom URL scheme
   } catch (error) {
